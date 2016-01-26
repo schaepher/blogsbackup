6baefd7998b7c5f21bda31b53ffb05c53ce9f46d
项塔兰 <qianlxc@126.com>
2016-01-26 17:14:25 +0800

    initial commit

---
+++ cnblogbackup/cnblogbackup/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Codaxy.WkHtmlToPdf;
+using MetroFramework.Forms;
+
+namespace cnblogbackup
+{
+    public partial class MainForm : MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            PdfConvert.ConvertHtmlToPdf(new PdfDocument
+            {
+                Url = "http://www.cnblogs.com/SivilTaram",
+                HeaderLeft = "[title]",
+                HeaderRight = "[date] [time]",
+                FooterCenter = "Page [page] of [topage]"
+
+            }, new PdfOutput
+            {
+                OutputFilePath = "test.pdf"
+            });
+        }
+    }
+}
